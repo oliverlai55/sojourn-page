3f75f45376eb3706d2d47a2711f53d76933c87fd
Wei lun Lai <oliverlai55@gmail.com>
2016-01-06 11:13:37 -0500

    edit php file

---
+++ index.php
@@ -0,0 +1,147 @@
+<?php
+	$connection = mysql_connect("127.0.0.1", "phpland", "x");
+
+	if (!$connection){
+		die('Could not connect: ' . mysql_error());
+	}
+
+	//make foo the current db
+	$db_selected = mysql_select_db('phpland', $connection);
+	if(!$db_selected){
+		die('Can\'t use phpland : ' . mysql_error());
+	}else{
+		print 'success!';
+	}
+
+	$query = "SELECT * FROM about WHERE section = 'header'";
+	// We have a var called query with our query in it
+	$result = mysql_query($query);
+	//We now have a mysql object called result
+	if(!$result){
+		die('Query error: ' . mysql_error());
+	}
+	
+	//prints out the error
+
+	// print $result;
+
+	while ($row = mysql_fetch_assoc($result)){
+		$section = $row["section"];
+		$rows[$section] = $row['content'];
+		//this is the same as array_push, it pushes to the array
+		//every single time it runs, it goes down the row, until the last row
+		//$rows is an array!! And we can access the item with key
+	}
+	print_r($rows);
+	exit;
+
+
+	// print "<pre>";
+	// print_r ($row);
+	$header_content = $row[0]['content'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="page-header col-sm-12">
+				<div class="col-sm-6"></div>
+				<h4 id="header-font" class="col-sm-2"><small>Prepare for Your Adventure</small></h4>
+				<img class="col-sm-0.5" src="http://q.b5z.net/zirw/h495b/i/t/w/integration/social/icons/fcc1/tw_32x32.png">
+				<img class="col-sm-0.5" src="http://q.b5z.net/zirw/h495b/i/t/w/integration/social/icons/fcc1/fb_32x32.png">
+				<img class="col-sm-0.5" src="http://q.b5z.net/zirw/h495b/i/t/w/integration/social/icons/fcc1/ig_32x32.png">
+				<h4 class="col-sm-1" id="header-feedback"><small>Feedback</small></h4>
+				<div class="col-sm-1.5"></div>
+			</div>
+		</div>
+		<div class="row">
+			<nav id="top-nav" class="navbar navbar-default">
+				<div class="container-fluid">
+					<div class="navbar-header col-sm-12">
+						<a class="navbar-brand col-sm-6" href="#">
+							<img id="sojourn-adven-header-img" src="http://pq.b5z.net/i/u/10099375/i/sojourn_logo_header.png">
+						</a>
+						<ul id="sojourn-nav" class="nav navbar-nav col-sm-6">
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Programs</a></li>
+							<li><a href="#">Leadership</a></li>
+							<li><a href="#">Contact Us</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+		<div class="row col-sm-12">
+			<div class="col-sm-2"></div>
+			<div class="about-wrapper col-sm-8">
+				<div class="left-wrapper col-sm-9">
+					<div class="col-sm-12">
+						<img id="about-title-image" class="col-sm-1" src="http://pq.b5z.net/zirw/317/i/u/10099375/i/menu/tico1.png">
+						<h2 class="col-sm-11" id="about-header">
+
+
+<?php
+
+	$string = "Hello World";
+	print '<h2>' . $string . '</h2>';
+	print $string;
+	print "HELLO WORLD";
+
+?>
+</h2>
+					</div>
+					<img id="sojourn-image" class="col-sm-12" src="http://pq.b5z.net/i/u/10099375/i/About_Sojourn_Header.jpg">
+					<!-- <p class="col-sm-8">Our lives take us on many journeys; with our careers, families, sports teams, schools, etc...  At Sojourn, our desire is to provide a safe and fun environment to Sojourn from these life journeys for a brief period of time in order to reflect, gain new insight, and enter back into our journeys with new perspective.</p> -->
+					
+					<?php print $rows['header']; ?>
+
+
+					<div class="image-box col-sm-4">
+						<img class="left-rail-image col-sm-12" src="http://pq.b5z.net/i/u/10099375/i/Fox_5_Video_of_Sojourn.png">
+						<p class="left-image-descrip col-sm-12">Check out this video about us on Fox 5</p>
+					</div>
+					<h2 class="col-sm-12 header-titles">Location</h2>
+					<p class="col-sm-8">Most Sojourn programming takes place on the property of Perimeter church in John's Creek, GA. The property is located at the corner of Old Alabama Rd. and Hwy 141/Medlock Bridge Rd. <br><br>If location is a challenge, ask us about off-site adventures and portable programs.</p>
+					<div class="image-box col-sm-4">
+						<img class="left-rail-image col-sm-12" src="http://pq.b5z.net/i/u/10099375/i/Get_Directions_to_Ropes_Course_Button.png">
+					</div>
+					<h2 class="header-titles col-sm-12">ACCT Membership</h2>
+					<p class="col-sm-8">The Association for Challenge Course Technology is recognized as the leading professional resource for those who create safe and effective challenge course programs that facilitate learning and positive change in individuals, groups, and communities. We are an organizational member of ACCT and our products and services are independent crom those offered by ACCT.  We are not an agent of ACCT nor does the ACCT warrant, endorse, or approve any product or service offered by us.  Sojourn operates under the standards set by ACCT.  All of our staff members are training according to ACCT standards and our key staff members have advanced ACCT certification.</p>
+					<div class="image-box col-sm-4">
+						<img class="left-rail-image col-sm-12" src="http://pq.b5z.net/i/u/10099375/i/ACCT_Logo_Button.png">
+					</div>
+					<h2 class="header-titles col-sm-12">Pricing</h2>
+					<p class="col-sm-12">Pricing varies depending on the length of your adventure, size of your group, and the activities necessary to attain certain group goals and objectives.  For full pricing and further details about booking a course, please contact the Sojourn office</p>
+					<p id="note-text" class="col-sm-12">Note:  Sojourn currently focuses exclusively on team building with pre-existing groups and therefore does not allow individual sign-ups for zip lining, birthday parties, or similar outings.</p>
+
+				</div>
+
+				<div class="right-wrapper col-sm-3">
+					<img class="right-rail-image col-sm-12" src="http://pq.b5z.net/i/u/10099375/i/Get_a_Quote.png">
+					<img class="right-rail-image col-sm-12" src="http://pq.b5z.net/i/u/10099375/i/Photo_Gallery_Button.png">
+					<img class="right-rail-image col-sm-12" src="http://pq.b5z.net/i/u/10099375/i/video_tour_button.png">
+					<img class="right-rail-image col-sm-12" src="http://pq.b5z.net/i/u/10099375/i/Prepare_for_Your_Event_Sidebar_Button.png">
+				</div>
+			</div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12" id="footer">
+				<p class="col-sm-12">9500 Medlock Bridge Rd.  |  Johns Creek, GA 30097  |  Phone: 678.405.2106   |   Email: sojourn@perimeter.org</p>
+				<img id="footer-img1" src="http://q.b5z.net/i/u/10099375/i/acct_logo_footer.png">
+				<img id="footer-img2" src="http://q.b5z.net/i/u/10099375/i/staff_login_button_footer.png">
+			</div>
+		</div>
+
+	</div>
+</body>
+</html>
